ecf1cccfa0672ed598fb48ea8e2631c9a9fea1c2
YuFireWhipser <yu.whisper.personal@gmail.com>
2025-02-14 14:59:08 +0800

    chore: Add use_default_config param

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,51 @@
+use std::path::PathBuf;
 use std::thread;
 use std::time::Duration;
 
 use blur::http::http_server::get_default_storage_path;
 use blur::{core::config::config_loader, http::http_manager::HttpManager};
 use clap::Parser;
+use std::env;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    #[arg(short, long, value_name = "FILE")]
+    #[arg(
+        short,
+        long,
+        value_name = "CONFIG FILE PATH",
+        conflicts_with = "use_default_config"
+    )]
     config_path: Option<String>,
+
+    #[arg(short, long, value_name = "USE DEFAULT CONFIG")]
+    use_default_config: bool,
 }
 
 fn main() {
     let args = Args::parse();
-
     let storage_path = get_default_storage_path();
 
-    let root_ctx = config_loader::load_config(
+    let config_path = if args.use_default_config {
+        let mut cargo_manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+        cargo_manifest_dir.push("config");
+        cargo_manifest_dir.push("default");
+        Some(cargo_manifest_dir.into_os_string().into_string().unwrap()) //轉成String
+    } else {
+        args.config_path
+    };
+
+    let root_ctx = match config_loader::load_config(
         storage_path.to_str().unwrap(),
-        args.config_path.as_deref(), // Convert Option<String> to Option<&str>
+        config_path.as_deref(), 
         vec!["http".to_string()],
-    )
-    .unwrap();
+    ) {
+        Ok(ctx) => ctx,
+        Err(e) => {
+            eprintln!("Error loading config: {}", e);
+            return;
+        }
+    };
 
     let http_block = root_ctx
         .children
